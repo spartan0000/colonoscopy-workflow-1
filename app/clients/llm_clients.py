ac1886ad8fdf955d9e8bdb2402d38604a36ba9d9
David Lee <doctor.dlee@gmail.com>
2026-03-02 21:14:53 +1300

    add new rules

---
+++ app/clients/llm_clients.py
@@ -0,0 +1,25 @@
+from openai import OpenAI, AzureOpenAI, AsyncAzureOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+chat_client = AsyncAzureOpenAI(
+    api_version = os.getenv('AZURE_GPT_API_VERSION'),
+    api_key = os.getenv('AZURE_OPENAI_API_KEY'),
+    azure_endpoint = os.getenv('AZURE_ENDPOINT'),
+)
+
+embedding_client = AsyncAzureOpenAI(
+    api_version = os.getenv('AZURE_EMBEDDING_API_VERSION'),
+    api_key = os.getenv('AZURE_OPENAI_API_KEY'),
+    azure_endpoint = os.getenv('AZURE_ENDPOINT'),
+)
+
+hnz_client = AsyncAzureOpenAI(
+    api_version = os.getenv('HNZ_API_VERSION'),
+    api_key = os.getenv('HNZ_API_KEY'),
+    azure_endpoint = os.getenv('HNZ_ENDPOINT')
+)
