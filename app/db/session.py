60c4500267ee23e42003fe0866e8e80fcf6c36b6
David Lee <doctor.dlee@gmail.com>
2026-05-23 22:43:42 +1200

    edit readme

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +11,8 @@ engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 #session dependency
 
